c581dae0b8a41f84c7635a0932523ada42662d37
nanlei <36439285+nanlei2000@users.noreply.github.com>
2019-08-05 13:03:20 +0800

    feat:实现 some 和 every

--- index.js
+++ index.js
@@ -18,8 +18,30 @@ function filter(list, fn) {
     return fn(cur, index, array) ? [...prev, cur] : prev
   }, [])
 }
+/**
+ *  implement every user `Array.prototype.reduce`(without break)
+ * @param {any[]} list
+ * @param {Function} fn
+ */
+function every(list, fn) {
+  return list.reduce((prev, cur, index, array) => {
+    return prev && fn(cur, index, array)
+  }, true)
+}
+/**
+ *  implement some user `Array.prototype.reduce`(without break)
+ * @param {any[]} list
+ * @param {Function} fn
+ */
+function some(list, fn) {
+  return list.reduce((prev, cur, index, array) => {
+    return prev || fn(cur, index, array)
+  }, false)
+}
 
 module.exports = {
   map,
   filter,
+  every,
+  some,
 }
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { map, filter } = require('./index').default
+const { map, filter, every, some } = require('./index')
 const assert = require('assert')
 const list = [1, 2, 3]
 const stringList = ['1', '2', '3']
@@ -7,11 +7,39 @@ assert.deepStrictEqual(
   map(list, item => {
     return String(item)
   }),
-  stringList
+  list.map(item => {
+    return String(item)
+  })
 )
 assert.deepStrictEqual(
   filter(list, item => {
     return item > 2
   }),
-  listGreaterThanTwo
+  list.filter(item => {
+    return item > 2
+  })
+)
+assert.deepStrictEqual(
+  every(list, item => {
+    return item > 0
+  }),
+  list.every(item => {
+    return item > 0
+  })
+)
+assert.deepStrictEqual(
+  every(list, item => {
+    return item > 1
+  }),
+  list.every(item => {
+    return item > 1
+  })
+)
+assert.deepStrictEqual(
+  some(list, item => {
+    return item > 1
+  }),
+  list.some(item => {
+    return item > 1
+  })
 )
